7d1bcbe37243a61f29728141eb973a2f7f0dce02
luisreys <theluisitorey@usal.es>
2017-01-18 14:23:08 +0200

    One month
    Show the first month is ready and working.

--- program.js
+++ program.js
@@ -9,25 +9,28 @@ var mm = currentDate.getMonth();  // 0 - 11
 var yyyy = currentDate.getFullYear();  // yyyy
 var weekDay = currentDate.getDay();  // 0 - 6
 
-var x;
+var x, y;
 var aux1, aux2 = 0;
 var month = [];
+var tmp = 1;
 
 aux2 = weekDay;
-for (aux1 = dd; aux1 > 0; (aux1 = aux1 - 1)) {
+//console.log("aux2: " + aux2 + " dd: " + dd);
+for (aux1 = dd - 1; aux1 > 0; aux1--) {
     if (aux2 > 0) {
         aux2 = aux2 - 1;
     } else {
         aux2 = 6;
     }
+    //console.log(aux1 + ": " + aux2 + ", ");
 }
 
 // Now we know what the first day of this month was.
 
-for (x = 0; x < num_days[mm]; (x = x + 1)) {
-    
+for (x = 0; x < num_days[mm]; x++ ){
+
     month[x] = aux2;
-    
+
     if (aux2 < 6) {
         aux2 = aux2 + 1;
     }else{
@@ -36,3 +39,18 @@ for (x = 0; x < num_days[mm]; (x = x + 1)) {
 }
 
 //Here I already should have all the days with the weekly day.
+
+/* for (var i = 0; i < num_days[mm]; i++) {
+  console.log(i + " " + month[i]);
+}  */
+
+for(y = month[0] + 1; y <= 7; y++){
+  document.getElementById(String('pos'+ 1 + y)).innerHTML = tmp;
+  tmp++;
+}
+for (x = 2; x < 7; x++) {
+  for (y = 1; y <= 7; y++) {
+    document.getElementById(String('pos'+ x + y)).innerHTML = tmp;
+    tmp++;
+  }
+}
